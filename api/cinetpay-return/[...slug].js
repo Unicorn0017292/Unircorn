5e73626305c690c9aa2bd659db2eaf7636952ca6
Unicorn0017292 <unircorn237@gmail.com>
2025-07-18 15:36:58 +0000

    Update [...slug].js

--- api/cinetpay-return/[...slug].js
+++ api/cinetpay-return/[...slug].js
@@ -1,13 +1,21 @@
 export default async function handler(req, res) {
-  const { slug } = req.query;  // ['join', '7jucil']
-
-  const dynamicPath = slug ? slug.join('/') : '';
-
+  console.log('Method:', req.method);
+  console.log('Query:', req.query);
+  console.log('Body:', req.body);
+  
+  const { slug } = req.query;
+  
+  // Construire le chemin dynamique
+  const dynamicPath = Array.isArray(slug) ? slug.join('/') : (slug || '');
   const redirectUrl = `https://unircorn.net/${dynamicPath}`;
-
+  
+  console.log('Redirecting to:', redirectUrl);
+  
+  // Gérer POST et GET
   if (req.method === 'POST' || req.method === 'GET') {
+    // Redirection 302 vers la page FlutterFlow
     return res.redirect(302, redirectUrl);
   } else {
-    res.status(405).send('Method Not Allowed');
+    res.status(405).json({ error: 'Method Not Allowed' });
   }
 }
