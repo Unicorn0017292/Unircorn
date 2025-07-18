75bd4256dc4694ce180e691ccb5645d2264db723
Unicorn0017292 <unircorn237@gmail.com>
2025-07-18 16:37:25 +0000

    Update [...slug].js

--- api/cinetpay-return/[...slug].js
+++ api/cinetpay-return/[...slug].js
@@ -1,14 +1,6 @@
 export default async function handler(req, res) {
-  // req.query.slug est un tableau des segments
   const { slug } = req.query;
   const dynamicPath = slug ? slug.join('/') : '';
-  // URL finale vers ta page FlutterFlow
   const redirectUrl = `https://unircorn.net/${dynamicPath}`;
-
-  if (req.method === 'POST' || req.method === 'GET') {
-    // Redirection 302 vers la page dynamique
-    return res.redirect(302, redirectUrl);
-  }
-  // Toute autre méthode : 405
-  res.status(405).send('Method Not Allowed');
+  return res.redirect(302, redirectUrl);
 }
